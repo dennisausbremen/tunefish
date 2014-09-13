8b7265608ce592ee7528569e4be066e56d0f5942
Alex vBK <avbk@neusta.de>
2014-09-13 23:18:11 +0200

    Band-Felder

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy_utils import PasswordType
+from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy_utils import PasswordType, URLType
 
 from server.database import db_session
 
@@ -14,7 +15,13 @@ class Band(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
+    emailConfirmed = Column(Boolean, default=False)
     password = Column(PasswordType(schemes=['pbkdf2_sha512']))
+    descp = Column(String)
+    website = Column(URLType)
+    youtube_id = Column(String)
+    phone = Column(String)
+    address = Column(String)
 
     def __init__(self, name=None, email=None):
         self.name = name
