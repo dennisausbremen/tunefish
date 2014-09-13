1d3229e5e38f6e503426af0636f6dbd00d41baa9
Alex vBK <avbk@neusta.de>
2014-09-13 23:36:54 +0200

    "secret" key

--- server/tunefish.py
+++ server/tunefish.py
@@ -8,6 +8,8 @@ from server.models import Band
 app = Flask(__name__)
 app.debug = True
 app.register_blueprint(bands, url_prefix='/bands')
+app.secret_key = "this_should_be_way_more_secret_like_urandom.its_only_static_for_debug_reasons"
+
 
 init_db()
 
