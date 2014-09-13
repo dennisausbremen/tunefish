921172ae8aa1d4ef107b7ceaa3340f46458c4ce9
Alex vBK <avbk@neusta.de>
2014-09-14 00:36:00 +0200

    ajax session

--- server/bands.py
+++ server/bands.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import Blueprint, request, jsonify, session
 from sqlalchemy.exc import IntegrityError
 
@@ -9,7 +11,7 @@ from server.models import Band
 BAND_ID = 'band_id'
 
 bands = Blueprint('bands', __name__, template_folder='templates')
-
+ajax_session = {}
 
 @bands.route('/')
 def show():
@@ -18,17 +20,17 @@ def show():
     else:
         return 'unkown'
 
-
 @bands.route('/login', methods=['POST'])
 def login():
+    auth_token = None
     if 'login' in request.form and 'password' in request.form:
         login = request.form['login']
         password = request.form['password']
         band = Band.query.filter(Band.login == login).first()
         if band:
             if band.password == password:
-                session[BAND_ID] = band.id
-                session.modified = True
+                auth_token = str(uuid.uuid4())
+                session[auth_token] = band.id
                 result = 'success'
             else:
                 result = 'bad_password'
@@ -37,27 +39,29 @@ def login():
     else:
         result = 'data_missing'
 
-    return jsonify(result=result)
+    return jsonify(result=result, auth_token=auth_token)
 
 
-@bands.route('/logout')
+@bands.route('/logout', methods=['POST'])
 def logout():
-    session.pop(BAND_ID)
+    auth_token = request.form['auth_token']
+    ajax_session.pop(auth_token, None)
 
 
 @bands.route('/register', methods=['POST'])
 def register():
+    auth_token = None
     if 'login' in request.form and 'password' in request.form:
         try:
             band = Band(request.form['login'], request.form['password'])
             db_session.add(band)
             db_session.commit()
-            session[BAND_ID] = band.id
-            session.modified = True
+            auth_token = str(uuid.uuid4())
+            session[auth_token] = band.id
             result = 'success'
         except IntegrityError as e:
             result = 'already_exists'
     else:
         result = 'data_missing'
 
-    return jsonify(result=result)
+    return jsonify(result=result, auth_token=auth_token)
