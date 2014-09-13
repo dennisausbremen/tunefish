6d28f7a2e9d83d5cbf1536a5d69bbea7e5cfa354
Alex vBK <avbk@neusta.de>
2014-09-13 23:47:52 +0200

    login

--- server/bands.py
+++ server/bands.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, session
 
 from server.models import Band
 
@@ -10,3 +10,24 @@ bands = Blueprint('bands', __name__, template_folder='templates')
 @bands.route('/<id>')
 def show(id):
     return Band.query.filter(Band.id == id).first().name
+
+
+@bands.route('/login', methods=['POST'])
+def login():
+    login = request.args.get('login', None, type=str)
+    password = request.args.get('password', None, type=str)
+    if login and password:
+        band = Band.query.filter(Band.login == 'login').first()
+        if band:
+            if band.password == password:
+                result = 'success'
+                session['band_id'] = band.id
+            else:
+                result = 'bad_password'
+        else:
+            result = 'unkown'
+    else:
+        result = 'data_missing'
+
+    return jsonify(result=result)
+
