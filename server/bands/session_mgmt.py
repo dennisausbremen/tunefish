127ee3cfe69f9ea0a5803d664f3dbcf64d53590c
Alexander von Bremen-Kühne <avbk@neusta.de>
2014-11-29 15:49:51 +0100

    Refactoring: removed duplicated code

--- server/bands/session_mgmt.py
+++ server/bands/session_mgmt.py
@@ -10,57 +10,66 @@ from server.bands.forms import LoginForm, RegistrationForm
 
 from server.models import Band, db
 
+MAIL_BODY = u"""Hallo %s,
 
-session_mgmt = Blueprint('bands.session', __name__, template_folder='../../client/views/bands')
+
+willkommnen bei der Sommerfest Auswahl. Um deine Bewerbung abschließen zu können, musst du zuerst deine E-Mail
+bestätigen. Klick hierzu einfach auf folgenden Link: %sbands/confirm/%d
+
+
+Viele Grüße
+SoFe Orga '15
+"""
 
 
 class Index(MethodView):
-    def render(self, loginForm, regForm):
-        return render_template('loginAndRegister.html', loginForm=loginForm, registerForm=regForm)
+    def __init__(self):
+        super(Index, self).__init__()
+        self.login_form = LoginForm()
+        self.registration_form = RegistrationForm()
+
+    def render(self):
+        return render_template('loginAndRegister.html', loginForm=self.login_form, registerForm=self.registration_form)
 
     def get(self):
-        return self.render(LoginForm(), RegistrationForm())
+        if 'bandId' in session:
+            return redirect(url_for('bands.profile.index'))
+        else:
+            return self.render()
 
 
 class Register(Index):
     def post(self):
-        regForm = RegistrationForm()
-        loginForm = LoginForm()
-        if regForm.validate_on_submit():
+        if self.registration_form.validate_on_submit():
             try:
-                band = Band(regForm.login.data, regForm.password.data)
-                band.email = regForm.email.data
+                band = Band(self.registration_form.login.data, self.registration_form.password.data)
+                band.email = self.registration_form.email.data
                 db.session.add(band)
                 db.session.commit()
                 msg = Message("Willkommen %s" % band.login,
                               sender="noreply@vorstrasse-bremen.de",
                               recipients=[band.email],
-                              body=u"""Hallo %s,
-
-willkommnen bei der Sommerfest Auswahl. Um deine Bewerbung abschließen zu können, musst du zuerst deine E-Mail
-bestätigen. Klick hierzu einfach auf folgenden Link: %sbands/confirm/%d""" % (band.login, request.url_root, band.id))
+                              body=MAIL_BODY % (band.login, request.url_root, band.id))
                 mailer.send(msg)
                 session['bandId'] = band.id
-                return redirect(url_for('bands.session.profile'))
+                return redirect(url_for('bands.profile.index'))
             except IntegrityError as e:
-                regForm.login.errors.append("Eine Band mit diesem Login existiert bereits")
-                return self.render(loginForm, regForm)
-        return self.render(loginForm, regForm)
+                self.registration_form.login.errors.append("Eine Band mit diesem Login existiert bereits")
+                return self.render()
+        return self.render()
 
 
 class Login(Index):
     def post(self):
-        regForm = RegistrationForm()
-        loginForm = LoginForm()
-        if loginForm.validate_on_submit():
-            band = Band.query.filter(Band.login == loginForm.login.data).first()
-            if band and band.password == loginForm.password.data:
+        if self.login_form.validate_on_submit():
+            band = Band.query.filter(Band.login == self.login_form.login.data).first()
+            if band and band.password == self.login_form.password.data:
                 session['bandId'] = band.id
                 return redirect(url_for('bands.profile.index'))
             else:
-                loginForm.login.errors.append("Unbekannter Login")
-                loginForm.password.errors.append("")
-        return self.render(loginForm, regForm)
+                self.login_form.login.errors.append("Unbekannter Login")
+                self.login_form.password.errors.append("")
+        return self.render()
 
 
 class Logout(MethodView):
@@ -69,7 +78,8 @@ class Logout(MethodView):
         return redirect(url_for('bands.session.index'))
 
 
-session_mgmt.add_url_rule('/', view_func=Index.as_view('index'))
+session_mgmt = Blueprint('bands.session', __name__, template_folder='../../client/views/bands')
+session_mgmt.add_url_rule('/', view_func=Index.as_view('index'), methods=['GET'])
+session_mgmt.add_url_rule('/', view_func=Login.as_view('login'), methods=['POST'])
 session_mgmt.add_url_rule('/register', view_func=Register.as_view('register'))
-session_mgmt.add_url_rule('/login', view_func=Login.as_view('login'))
 session_mgmt.add_url_rule('/logout', view_func=Logout.as_view('logout'))
