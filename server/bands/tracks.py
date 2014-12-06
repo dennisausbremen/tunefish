203d109a0ad773ca451b05ee8e111ba74232c857
Alexander von Bremen-Kühne <avbk@neusta.de>
2014-12-06 15:50:22 +0100

    Track Upload is now AJAX

--- server/bands/tracks.py
+++ server/bands/tracks.py
@@ -5,40 +5,30 @@ from uuid import uuid4
 from flask import Blueprint, redirect, url_for, request, flash
 from flask.templating import render_template
 from server.app import trackPool
-from server.bands import RestrictedBandPage
+from server.bands import RestrictedBandPage, AjaxForm, AjaxException
 from server.bands.forms import TrackUploadForm
 
 from server.models import db, Track
 
-
-class TrackPage(RestrictedBandPage):
+class TrackUpload(RestrictedBandPage, AjaxForm):
     def __init__(self):
         super(RestrictedBandPage, self).__init__()
-        self.uploadForm = TrackUploadForm()
-
-    def render(self):
-        tracks = self.band.tracks.all()
-        return render_template('tracks.html', uploadForm=self.uploadForm, tracks=tracks)
-
-    def get(self):
-        return self.render()
+        super(AjaxForm, self).__init__()
+        self.form = TrackUploadForm()
 
 
-class TrackUpload(TrackPage):
-    def post(self):
-        if self.uploadForm.validate_on_submit():
-            if len(self.band.tracks.all()) == 5:
-                flash(u'Es dürfen nur maximal 5 Demo-Songs hochgeladen werden.', 'error')
-            else:
-                track = Track()
-                track.band_id = self.band.id
-                trackFilename = str(self.band.id) + '_' + str(uuid4()) + '.mp3'
-                track.filename = trackPool.save(request.files[self.uploadForm.audioFile.name], name=trackFilename)
-                track.trackname = self.uploadForm.trackname.data
-                db.session.add(track)
-                db.session.commit()
-                flash('Song "%s" erfolgreich hochgeladen.' % track.trackname, 'info')
-        return self.render()
+    def on_submit(self):
+        if self.band.tracks.count() == 5:
+            raise AjaxException(u'Es dürfen nur maximal 5 Demo-Songs hochgeladen werden.')
+        else:
+            track = Track()
+            track.band_id = self.band.id
+            trackFilename = str(self.band.id) + '_' + str(uuid4()) + '.mp3'
+            track.filename = trackPool.save(request.files[self.form.audioFile.name], name=trackFilename)
+            track.trackname = self.form.trackname.data
+            db.session.add(track)
+            db.session.commit()
+            return {'track': render_template("track_item.html", track=track) }
 
 
 class TrackDelete(RestrictedBandPage):
@@ -52,6 +42,5 @@ class TrackDelete(RestrictedBandPage):
 
 
 tracks = Blueprint('bands.tracks', __name__, template_folder='../../client/views/bands')
-tracks.add_url_rule('/tracks', view_func=TrackPage.as_view('index'))
 tracks.add_url_rule('/tracks', view_func=TrackUpload.as_view('upload'))
 tracks.add_url_rule('/tracks/delete/<int:track_id>', view_func=TrackDelete.as_view('delete'))
