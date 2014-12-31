962a71a2596e0467d6e1491d6e0de58e2d55cb71
Eike Broda <ebroda@fb3.uni-bremen.de>
2014-12-31 15:39:15 +0100

    Remove some functions and imports that are no longer in use

--- server/bands/__init__.py
+++ server/bands/__init__.py
@@ -1,17 +1,11 @@
 # coding=utf-8
 
-from flask import Blueprint, render_template
-from flask.views import MethodView
+from flask import Blueprint
 from server.bands.image import ImageUpload
 from server.bands.profile import Confirm, ProfileUpdate, Onepager, ResendConfirmMail, SubmitProfile
 from server.bands.session_mgmt import LoginAndRegister, Login, Register, Logout
 from server.bands.techrider import TechriderUpload
 from server.bands.tracks import TrackUpload, TrackDelete
-
-
-class JavaScript(MethodView):
-    def get(self):
-        return render_template('onepager_ajax.js')
 
 
 band_blueprint = Blueprint('bands', __name__, template_folder='../../client/views/bands')
@@ -26,7 +20,6 @@ band_blueprint.add_url_rule('/submit', view_func=SubmitProfile.as_view('profile.
 band_blueprint.add_url_rule('/resend-confirm', view_func=ResendConfirmMail.as_view('profile.resend'))
 band_blueprint.add_url_rule('/profile', view_func=Onepager.as_view('profile.index'), methods=['GET'])
 band_blueprint.add_url_rule('/profile', view_func=ProfileUpdate.as_view('profile.update'), methods=['POST'])
-# band_blueprint.add_url_rule('/profile.js', view_func=JavaScript.as_view('profile.bandjs'), methods=['GET'])
 
 band_blueprint.add_url_rule('/images', view_func=ImageUpload.as_view('image.upload'))
 band_blueprint.add_url_rule('/techrider', view_func=TechriderUpload.as_view('techrider.upload'))
