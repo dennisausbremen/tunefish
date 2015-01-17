e9a2114d2d55e9bf07fbbe97f96420cb2b974d50
Alexander von Bremen-Kühne <avbk@neusta.de>
2015-01-17 16:08:22 +0100

    Added thumbnails

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.images import Images
 from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 from celery import Celery
 
@@ -26,6 +27,7 @@ app.debug = True
 app.secret_key = "this_should_be_way_more_secret_like_urandom.its_only_static_for_debug_reasons"
 app.config.update(SETTINGS)
 celery = make_celery(app)
+images = Images(app)
 
 trackPool = UploadSet('trackPool', ('mp3',))
 imagePool = UploadSet('imagePool', IMAGES)
