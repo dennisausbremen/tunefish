2ea535f27ba195d3febab729fdad7da7fd49d184
Eike Broda <ebroda@fb3.uni-bremen.de>
2015-01-25 18:48:10 +0100

    Remove single band view in non-ajax version

--- server/vote/band_vote.py
+++ server/vote/band_vote.py
@@ -12,15 +12,3 @@ from server.vote.session_mgmt import RestrictedUserPage
 class BandApp(RestrictedUserPage):
     def get(self):
         return render_template('app.html')
-
-
-class BandDetails(RestrictedUserPage):
-    def get(self, band_id):
-        band = Band.query.get(band_id)
-        if band:
-            comment_form = CommentForm()
-            comment_form.band_id.data = band_id
-            return render_template('band_view.html', band=band, comment_form=comment_form)
-        else:
-            flash('Es existiert keine Band mit dieser ID', 'error')
-            return redirect(url_for('vote.bands.list'))
