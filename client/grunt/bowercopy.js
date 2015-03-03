d5d61f00b6f6b489c7e090dbfb867d67191c45e8
Eike Broda <ebroda@fb3.uni-bremen.de>
2015-03-03 23:06:28 +0100

    use ember.min

--- client/grunt/bowercopy.js
+++ client/grunt/bowercopy.js
@@ -10,7 +10,7 @@ module.exports = {
             'vendor/bands/01-slick.min.js': 'slick.js/slick/slick.min.js',
             'vendor/voters/01-handlebars.min.js': 'handlebars/handlebars.min.js',
             'vendor/voters/02-mixitup.min.js': 'mixitup2/build/jquery.mixitup.min.js',
-            'vendor/voters/03-ember.min.js': 'ember/ember.js',
+            'vendor/voters/03-ember.min.js': 'ember/ember.min.js',
             'vendor/voters/04-ember-data.min.js': 'ember-data/ember-data.min.js'
         }
     }
