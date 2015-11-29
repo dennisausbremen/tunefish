fccd8ef0839504d6e0391e65396ee4f91984fe43
Eike Broda <ebroda@fb3.uni-bremen.de>
2015-11-29 01:38:19 +0100

    diverses

--- client/app/15tunefish/app.js
+++ client/app/15tunefish/app.js
@@ -1,4 +1,4 @@
-var tunefish = angular.module('tutorialApp', ['ngAnimate', 'ngRoute', 'angularSoundManager']);
+var tunefish = angular.module('tunefishApp', ['ngRoute', 'angularSoundManager']);
 var apiBase = 'http://localhost:5000/vote/api/v2/';
 
 tunefish.config(function ($routeProvider) {
@@ -13,9 +13,42 @@ tunefish.config(function ($routeProvider) {
 });
 
 
-tunefish.controller('MainCtrl', function($scope, $location) {
+tunefish.controller('MainCtrl', function($scope, $location, BandFactory, angularPlayer, JwtFactory) {
     $scope.searchBand = function() {
         $location.path('/');
+    };
+
+    $scope.minRating = 0;
+    $scope.maxRating = 5;
+
+    $scope.setRating = function(min, max) {
+        $scope.minRating = min;
+        $scope.maxRating = max;
+    }
+
+    $scope.allToPlaylist = function(filteredBands) {
+        angular.forEach(filteredBands, function (band) {
+            BandFactory.getBand(band.id).then(function(fullBand) {
+                fullBand.tracks.forEach(function(track) {
+                    track.url = track.url + '?Authorization=JWT%20' + JwtFactory.getToken();
+                    angularPlayer.addTrack(track);
+                });
+            });
+
+        });
+    }
+
+});
+
+tunefish.filter('ownRating', function() {
+    return function (values, minRating, maxRating) {
+        var filtered = [];
+        angular.forEach(values, function (value) {
+            if (value.rating <= maxRating && value.rating >= minRating) {
+                filtered.push(value);
+            }
+        });
+        return filtered;
     }
 });
 
@@ -27,7 +60,7 @@ tunefish.controller('LoginCtrl', function ($scope, $http, $sce, $location, JwtFa
         JwtFactory.determineToken(user).then(function (result) {
             if (result) {
                 $scope.feedback = $sce.trustAsHtml('<div class="alert alert-success">Login erfolgreich!</div>');
-                $location.path('/'); ///band/54');
+                $location.path('/'); //band/54');
             } else {
                 $scope.feedback = $sce.trustAsHtml('<div class="alert alert-danger">Falsche Zugangsdaten</div>');
             }
@@ -36,20 +69,21 @@ tunefish.controller('LoginCtrl', function ($scope, $http, $sce, $location, JwtFa
 
 });
 
-tunefish.controller('BandsCtrl', function ($scope, $location, Bands, JwtFactory) {
+tunefish.controller('BandsCtrl', function ($scope, $location, BandsFactory, JwtFactory) {
     if (JwtFactory.hasToken()) {
-        Bands.getBands().then(function (bands) {
+        BandsFactory.getBands().then(function (bands) {
             $scope.bands = bands;
         });
     }
 });
 
 
-tunefish.controller('BandCtrl', function ($scope, $routeParams, angularPlayer, $sce, Bands, BandFactory, JwtFactory) {
+tunefish.controller('BandCtrl', function ($scope, $routeParams, angularPlayer, $sce, BandsFactory, BandFactory, JwtFactory) {
 
     var band = {};
     if (JwtFactory.hasToken()) {
-        band = Bands.getBand($routeParams.bandID);;
+
+        band = BandsFactory.getBand($routeParams.bandID);
         $scope.band = band;
         $scope.loading = true;
 
@@ -58,11 +92,13 @@ tunefish.controller('BandCtrl', function ($scope, $routeParams, angularPlayer, $
             $scope.band = band;
 
             band.tracks.forEach(function(track) {
-                track.url = track.url + '?Authorization=JWT%20' + JwtFactory.getToken()
+                track.url = track.url + '?Authorization=JWT%20' + JwtFactory.getToken();
             });
 
             $scope.description = $sce.trustAsHtml(band.description);
             $scope.loading = false;
+
+
         });
     }
 });
@@ -113,11 +149,31 @@ tunefish.factory('JwtFactory', function ($http, $location) {
 
 
 tunefish.factory('BandFactory', function ($http, $q, $location, JwtFactory) {
+
+    var bandCache = [];
     var bands = [];
     return {
         getBand: function (bandId) {
+            // -- check if band is in local cache ...
+            // necessary for voting, as else the reference for the band object in the
+            // track objects is no longer given and so the votes on the band page aren't
+            // updated
+
+            if (bandId in bandCache) {
+                // TODO: But maybe reload the comments?
+                var deferred = $q.defer();
+                deferred.resolve(bandCache[bandId]);
+                return deferred.promise;
+            }
+
             var success = function (bandResponse) {
-                return bandResponse.data;
+                band = bandResponse.data;
+                band.tracks.forEach(function(track) {
+                    track.band = band;
+                });
+
+                bandCache[band.id] = band;
+                return band;
             };
 
             var fail = function (bandResponse) {
@@ -134,13 +190,13 @@ tunefish.factory('BandFactory', function ($http, $q, $location, JwtFactory) {
 });
 
 
-tunefish.factory('Bands', function ($http, $q, $location, JwtFactory) {
+tunefish.factory('BandsFactory', function ($http, $q, $location, JwtFactory) {
     var bands = [];
     return {
         getBand: function (bandId) {
             var singleBand = {artist: '', id: bandId};
 
-            bands.forEach(function (band) {
+            bands.forEach(function(band) {
                 if (band.id == bandId) {
                     singleBand = band;
                 }
@@ -171,8 +227,52 @@ tunefish.factory('Bands', function ($http, $q, $location, JwtFactory) {
             var deferred = $q.defer();
             deferred.resolve(bands);
             return deferred.promise;
+        },
+        setRating: function(bandId, rating) {
+             bands.forEach(function(band) {
+                if (band.id == bandId) {
+                    band.rating = rating;
+                }
+            });
         }
     };
+});
+
+tunefish.directive('voting', function(){
+  return {
+    restrict: 'E',
+    scope: {
+      band: '='
+    },
+    template: '<span ng-repeat="cnt in [1,2,3,4,5]"><star digit="cnt" band="band"></star></span>'
+  };
+});
+
+tunefish.directive('star', function($http, JwtFactory, BandsFactory) {
+   return {
+       restrict: 'E',
+       scope: {
+           digit: '=',
+           band: '='
+       },
+       link: function (scope, element) {
+        element.bind('click', function () {
+
+            var success = function() {
+                scope.band.rating = scope.digit;
+                BandsFactory.setRating(scope.band.id, scope.digit);
+            };
+
+            var fail = function() {
+                // TODO: notify the user?, check whether token was still valid
+                console.log('voting failed');
+            };
+
+            $http.put(apiBase + 'bands/' + scope.band.id + '/vote', {vote: scope.digit}, JwtFactory.getHeaders()).then(success, fail);
+        });
+       },
+       template: '<i class="glyphicon " ng-class="{\'glyphicon-star\': digit <= band.rating, \'glyphicon-star-empty\': digit > band.rating}"></i>'
+   };
 });
 
 
@@ -192,6 +292,21 @@ tunefish.directive('addAll', ['angularPlayer', function(angularPlayer) {
                 if (!angularPlayer.isPlayingStatus()) {
                     angularPlayer.play();
                 }
+            });
+        }
+    }
+}]);
+
+
+tunefish.directive('random', ['angularPlayer', function(angularPlayer) {
+    return {
+        restrict: "EA",
+        scope: {
+            songs: '=random'
+        },
+        link: function (scope, element) {
+            element.bind('click', function () {
+                alert('need to implement random');
             });
         }
     }
